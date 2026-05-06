6865894520327d12319ddded9726cd8417a190bc
Casper V. Kristensen <casper@vkristensen.dk>
2026-05-06 12:27:52 +0200

    Revert "claude: managed-settings.json using environment.etc"
    This reverts commit 0fec39867e8166fa83506edf7b06d46267f44e54.

--- container/claude.nix
+++ container/claude.nix
@@ -34,18 +34,6 @@
   };
 
   # https://code.claude.com/docs/en/settings
-  environment.etc."claude-code/managed-settings.json".text = builtins.toJSON {
-    # Disable commercials in git commits
-    attribution = {
-      commit = "";
-      pr = "";
-    };
-    # Default to the best model
-    model = "opus";
-    # yolo
-    permissions.defaultMode = "bypassPermissions";
-    skipDangerousModePermissionPrompt = true;
-  };
   systemd.tmpfiles.rules = let
     claudeJson = pkgs.writeText "claude.json" (builtins.toJSON {
       # Claude Code asks us to log in during onboarding. We may want to use
@@ -58,8 +46,21 @@
         };
       };
     });
+    managedSettingsJson = pkgs.writeText "managed-settings.json" (builtins.toJSON {
+      # Disable commercials in git commits
+      attribution = {
+        commit = "";
+        pr = "";
+      };
+      # Default to the best model
+      model = "opus";
+      # yolo
+      permissions.defaultMode = "bypassPermissions";
+      skipDangerousModePermissionPrompt = true;
+    });
   in [
     "C /root/.claude.json 0600 root root - ${claudeJson}"
+    "L+ /etc/claude-code/managed-settings.json - - - - ${managedSettingsJson}"
     # We must use AGENTS.md, rather than CLAUDE.md, since we patched the binary
     "L+ /root/.claude/AGENTS.md - - - - ${vars.AGENTS_md}"
   ];
